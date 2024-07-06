1411edc23268ef3b96a7c4daed296108b6e40118
Cytraen <60638768+Cytraen@users.noreply.github.com>
2024-07-06 12:40:45 -0400

    api X update
    also use dalamud's worldtoscreenpos

--- src/Game/GameObjects.cs
+++ src/Game/GameObjects.cs
@@ -1,4 +1,7 @@
-﻿using FFXIVClientStructs.FFXIV.Client.Game.Object;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using static FFXIVClientStructs.FFXIV.Client.UI.UI3DModule;
 
@@ -11,8 +14,9 @@ namespace AetherCompass.Game
 
         // Those that would be rendered on screen
         internal unsafe static ObjectInfo** SortedObjectInfoPointerArray
-            => UI3DModule != null
-            ? (ObjectInfo**)UI3DModule->SortedObjectInfoPointerArray : null;
+        => UI3DModule != null
+            ? (ObjectInfo**)Unsafe.AsPointer(ref MemoryMarshal.GetReference(UI3DModule->SortedObjectInfoPointers)) //(ObjectInfo**)UI3DModule->SortedObjectInfoPointerArray 
+            : null;
         internal unsafe static int SortedObjectInfoCount
             => UI3DModule != null ? UI3DModule->SortedObjectInfoCount : 0;
 
@@ -20,14 +24,14 @@ namespace AetherCompass.Game
         private unsafe static readonly GameObjectManager* gameObjMgr 
             = GameObjectManager.Instance();
         internal unsafe static GameObject* ObjectListFiltered
-            => (GameObject*)gameObjMgr->ObjectListFiltered;
+            => (GameObject*)&gameObjMgr->Objects;
         internal unsafe static int ObjectListFilteredCount
-            => gameObjMgr->ObjectListFilteredCount;
+            => gameObjMgr->Objects.GameObjectIdSortedCount;
 
         static GameObjects()
         {
             LogDebug($"UI3DModule @{(IntPtr)UI3DModule:X}");
-            LogDebug($"SortedObjectInfoPointerArray @{(IntPtr)UI3DModule->SortedObjectInfoPointerArray:X}");
+            // LogDebug($"SortedObjectInfoPointerArray @{(IntPtr)MemoryMarshal.GetReference(UI3DModule->SortedObjectInfoPointers):X}");
             LogDebug($"SortedObjectInfoCount = {SortedObjectInfoCount}");
         }
 #endif
